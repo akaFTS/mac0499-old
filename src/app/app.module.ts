80fc1d4fa89dbadb0df676bd2def02397e4fa6eb
Gustavo Silva <gustavo@cupertino.local>
2018-04-26 23:40:33 -0300

    Added header and propposal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { PropposalComponent } from './propposal/propposal.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    PropposalComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/propposal/propposal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-propposal',
+  templateUrl: './propposal.component.html',
+  styleUrls: ['./propposal.component.scss']
+})
+export class PropposalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
