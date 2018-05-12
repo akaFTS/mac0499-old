3bbe44c61edd7ca1d7edbaaf656fe1e597fea67c
Gustavo Silva <gustavo@cupertino.local>
2018-05-12 00:51:32 -0300

    Restyling, grid structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { PropposalComponent } from './propposal/propposal.component';
+import { PropposalComponent } from './information/propposal/propposal.component';
+import { InformationComponent } from './information/information.component';
+import { ActivitiesComponent } from './information/activities/activities.component';
 
+const appRoutes: Routes = [
+  { path: '', component: InformationComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    PropposalComponent
+    PropposalComponent,
+    InformationComponent,
+    ActivitiesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
