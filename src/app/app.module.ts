f56f8b72ad2dce3af1b4564bbe9af28a5936359e
Gustavo Silva <gustavo@cupertino.local>
2018-05-12 18:18:03 -0300

    Routing, icons, course page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { PropposalComponent } from './information/propposal/propposal.component';
 import { InformationComponent } from './information/information.component';
 import { ActivitiesComponent } from './information/activities/activities.component';
+import { CourseComponent } from './course/course.component';
+import { TimelineComponent } from './course/timeline/timeline.component';
+import { StudentsComponent } from './course/students/students.component';
+import { ProfessorsComponent } from './course/professors/professors.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 const appRoutes: Routes = [
-  { path: '', component: InformationComponent }
+  { path: '', component: CourseComponent },
+  { path: 'info', component: InformationComponent }
 ];
+library.add(fas);
 
 @NgModule({
   declarations: [
@@ -19,10 +28,16 @@ const appRoutes: Routes = [
     HeaderComponent,
     PropposalComponent,
     InformationComponent,
-    ActivitiesComponent
+    ActivitiesComponent,
+    CourseComponent,
+    TimelineComponent,
+    StudentsComponent,
+    ProfessorsComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
+    FontAwesomeModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
